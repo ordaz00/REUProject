3600738cc5f0bfeae901e602b536d727a802822d
ordaz00 <ordazjose00@gmail.com>
2022-04-04 17:28:49 -0700

    Automated the gitPlots.r function

--- gitPlots.r
+++ gitPlots.r
@@ -1,23 +1,28 @@
 library(ggplot2)
+names <- list("Ruby_On_Rails", "Lucene", "TensorFlow", "Pytorch")
+for(i in names){
+  csv1 = paste(i,"_Commit_Stats.csv",sep="")
+  csv2 = paste(i,"_Fraction_Stats.csv",sep="")
+  title1 =paste(i,"_Commit_Stats.pdf",sep="")
+  title2 =paste(i,"_Fraction_Stats.pdf",sep="")
 
-df = read.csv("TensorFlow_Commit_Stats.csv")
-list1 = list()
-for(i in 1:ncol(df)) {      
-  list1[[i]] <- df[ , i]    
-}
-names(list1)=colnames(df)
-#print(df)
-DF <- as.data.frame(list1)
-ggplot(DF, aes(num1)) + geom_histogram(bins=25) + ggtitle("Pytorch Commits Histogram") +labs(y= "Authors who made that many commits", x = "Number of Commits")
-ggsave("TensorFlow_Commit_Stats.pdf")
+  df = read.csv(csv1)
+  list1 = list()
+  for(i in 1:ncol(df)) {      
+   list1[[i]] <- df[ , i]    
+  }
+  names(list1)=colnames(df)
+  DF <- as.data.frame(list1)
+  ggplot(DF, aes(num1)) + geom_histogram(bins=25) + ggtitle("Pytorch Commits Histogram") +labs(y= "Authors who made that many commits", x = "Number of Commits")
+  ggsave(title1)
 
-df = read.csv("TensorFlow_Fraction_Stats.csv")
-list1 = list()
-for(i in 1:ncol(df)) {      
-  list1[[i]] <- df[ , i]    
+  df = read.csv(csv2)
+  list1 = list()
+  for(i in 1:ncol(df)) {      
+    list1[[i]] <- df[ , i]    
+  }
+  names(list1)=colnames(df)
+  DF <- as.data.frame(list1)
+  ggplot(DF, aes(num1)) + geom_histogram(bins=10) + ggtitle("Pytorch Fractions Histogram") +labs(y= "Authors at that fraction", x = "Fraction")
+  ggsave(title2)
 }
-names(list1)=colnames(df)
-#print(df)
-DF <- as.data.frame(list1)
-ggplot(DF, aes(num1)) + geom_histogram(bins=10) + ggtitle("Pytorch Fractions Histogram") +labs(y= "Authors at that fraction", x = "Fraction")
-ggsave("TensorFlow_Fraction_Stats.pdf")
